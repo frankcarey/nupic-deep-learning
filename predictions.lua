6b156a561696cd8f81c8dfba392a2cef63925092
Frank Carey <frank@zivtech.com>
2015-05-30 22:58:56 -0400

    Finally got Predictions class working right, and can return a subset of categories.

--- predictions.lua
+++ predictions.lua
@@ -1,19 +1,25 @@
-local Predictions = {
-  output = {}
-  predictions = {}
-}
+local Predictions = { output2 = nil; predictions = {} }
 
-function Predictions:init(output)
-  self.output = output:float()
+function Predictions:init(net_output)
+  something = 'test'
+  self.output2 = net_output:float()
+  local label = require 'overfeat_label'
+  for i=1,self.output2:size()[1] do
+    table.insert(self.predictions, {category_id=i, label=label[i], confidence=self.output2[i]})
+  end
 end
 
 -- output needs to be from output = net:forward(input)
-function Predictions:get()
-  local label = require 'overfeat_label'
-  for i=1,self.output:size()[1] do
-    table.insert(self.predictions, {category_id=i, label=label[i], confidence=self.output[i]})
+function Predictions:sort()
+  table.sort(self.predictions, function(a,b) return a.confidence > b.confidence end)
+end
+
+function Predictions:getN(limit)
+  local t={};
+  for i=1,limit do
+    t[i] = self.predictions[i]
   end
-  return table.sort(self.predictions, function(a,b) return a.confidence > b.confidence end)
+  return t
 end
 
 return Predictions
