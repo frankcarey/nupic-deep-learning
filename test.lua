d59bf0e74e1742125731c9cc82c271f49ec9b1ad
Frank Carey <frank@zivtech.com>
2015-05-31 02:39:41 -0400

    Only load files that exit, need to filter out '.' and '..' files from folder listing.

--- test.lua
+++ test.lua
@@ -159,10 +159,11 @@ ParamBank:close()
 ImgLoader = require 'ImgLoader'
 Predictions = require 'predictions'
 
-dirpath = "images"
+dirpath = "images/"
 
 if not dirpath or not paths.dirp(dirpath) then
   print ("directory " .. dirpath or "?" .. " isn't set or doesn't exist, using test images.");
+  dirpath = ""
   img_list = ImgLoader:list_iter({
     "lena",
     "bee.jpg"
@@ -173,12 +174,13 @@ else
 end
 
 for file in img_list do
-  img = ImgLoader:load(file);
-  break
+  img = ImgLoader:load(dirpath .. file);
+  if not img then goto continue end
   --itorch.image(img)
   output = net:forward(img)
   pred_a = Predictions:new{output = output}
   pred_a:init()
   pred_a:sort()
   print(pred_a:getN(3))
+  ::continue::
 end
